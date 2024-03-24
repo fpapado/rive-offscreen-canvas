b1f511ac7fcdb656c9dc179805bbaf7560623649
Fotis Papadogeorgopoulos <fotis.papadogeorgopoulos@wolt.com>
2024-03-24 13:36:15 +0200

    Tidy up BlockMainThreadButton

--- src/BlockMainThreadButton.tsx
+++ src/BlockMainThreadButton.tsx
@@ -1,16 +1,27 @@
+import { useState } from "react";
+
 function fibonacci(n: number): number {
   return n <= 1 ? n : fibonacci(n - 1) + fibonacci(n - 2);
 }
 
 export function BlockMainThreadButton() {
+  const [isBlocked, setBlocked] = useState(false);
   return (
     <button
       className="blockMainThreadButton"
       onClick={() => {
-        console.log(fibonacci(42));
+        if (isBlocked) {
+          return;
+        }
+        setBlocked(true);
+        setTimeout(() => {
+          fibonacci(42);
+          setBlocked(false);
+        }, 100);
       }}
+      aria-busy={isBlocked}
     >
-      Block main thread
+      {isBlocked ? "Blocking..." : "Block main thread"}
     </button>
   );
 }
